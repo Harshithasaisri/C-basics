1b03f9aa7ec16da80bcf1e4dbc88e7e036454bc3
Harshithasaisri <96580368+Harshithasaisri@users.noreply.github.com>
2021-12-30 11:37:01 +0530

    Add files via upload

---
+++ divsibility.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+ main()
+{
+	int a;
+	printf("enter integer a");
+	scanf("%d",&a);
+	if(a%1==0)
+	{
+		printf("a is divisible by 1\n");
+	}
+ if(a%2==0)
+	{
+		printf("a is divisible by 2\n");
+	}
+	 if(a%3==0)
+	{
+		printf("a is divisible by 3\n");
+	}
+	 if(a%4==0)
+	{
+		printf("a is divisible by 4\n");
+	}
+	 if(a%5==0)
+	{
+		printf("a is divisible by 5\n");
+	}
+	 if(a%6==0)
+	{
+		printf("a is divisible by 6\n");
+	}
+	 if(a%7==0)
+	{
+		printf("a is divisible by 7\n");
+	}
+	 if(a%8==0)
+	{
+		printf("a is divisible by 8\n");
+	}
+	if(a%9==0)
+	{
+		printf("a is divisible by 9");
+	}
+}
+
